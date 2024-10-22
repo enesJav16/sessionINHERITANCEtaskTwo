4bdd505a6d76e98be8f2e0ce6b6c33a2bdea55a8
enesJav16 <maratuenes@gmail.com>
2024-10-22 14:47:18 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,49 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner=new Scanner(System.in);
+//        Mother mother =new Mother();
+//        Daughter daughter1= new Daughter();
+//        mother.setName("Mother");
+//        daughter1.setName("Daughter");
+//
+//        System.out.println(mother.getName1());
+//        System.out.println(daughter1.toString());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Tech tech = new Tech();
+
+        Phone phone1 = new Phone("Note 10","Samsung","1.12.34",1,"black","12345");
+        Phone phone2 = new Phone();
+
+        Computer computer1 = new Computer("Ideapad","Lenovo","21.21.21",1,40);
+        Computer computer2 = new Computer();
+
+        SmartWatch watch1 = new SmartWatch("Smartwach","Samsung","1.6.5",0,200);
+        SmartWatch watch2 = new SmartWatch();
+
+
+        boolean check = true;
+        while (check) {
+            System.out.println("Chose device: ");
+            System.out.println("1: Phone\n2: Computer\n3: Watch");
+            int choseDevice= scanner.nextInt();
+            switch (choseDevice){
+                case 1:
+                   phone1.turnOnOff(phone1.getPassword());
+                   break;
+                case 2:
+                    computer1.turnOnOff(computer1.getName());
+                    break;
+                case 3:
+                    watch1.turnOnOff(watch1.getName());
+                    break;
+                default:
+                    System.out.println("Chose another device");
+                    break;
+            }
         }
     }
 }
